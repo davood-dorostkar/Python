0836b8ef9201962b2e98cf1e1ea64bd6b0c2235c
davood-dorostkar <dado2hacker@gmail.com>
2021-11-23 16:17:52 +0330

    rewrited as class

--- realpython.py
+++ realpython.py
@@ -2,28 +2,43 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Firefox()
-browser.implicitly_wait(5)
-browser.get('https://instagram.com/')
-sleep(5)
 
-passFile = open('pass.txt', "r")
-content = passFile.read()
-passFile.close()
-username = content.split('user=', 1)[1].split('pass=', 1)[0].strip()
-password = content.split('pass=', 1)[1].strip()
+class Scraper:
+    def __init__(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
-username_box = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
-password_box = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
-username_box.send_keys(username)
-password_box.send_keys(password)
-login_btn = browser.find_element(By.XPATH, "//button[@type='submit']")
-login_btn.click()
-sleep(5)
-save_btn = browser.find_element(By.XPATH, "//button[text()='Save Info']")
-save_btn.click()
-sleep(2)
-notNow_btn = browser.find_element(By.XPATH, "//button[text()='Not Now']")
-notNow_btn.click()
-sleep(5)
-browser.close()
+    def open(self, address):
+        self.browser.get(address)
+
+    def getUserPass(self, fileName):
+        passFile = open(fileName, "r")
+        content = passFile.read()
+        passFile.close()
+        self.username = content.split('user=', 1)[1].split('pass=', 1)[0].strip()
+        self.password = content.split('pass=', 1)[1].strip()
+
+    def login(self):
+        username_box = self.browser.find_element(By.CSS_SELECTOR, "input[name='username']")
+        password_box = self.browser.find_element(By.CSS_SELECTOR, "input[name='password']")
+        username_box.send_keys(self.username)
+        password_box.send_keys(self.password)
+        login_btn = self.browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+        login_btn.click()
+        save_btn = self.browser.find_element(By.XPATH, "//button[text()='Save Info']")
+        save_btn.click()
+        sleep(2)
+        notNow_btn = self.browser.find_element(By.XPATH, "//button[text()='Not Now']")
+        notNow_btn.click()
+
+    def closeBrowser(self):
+        self.browser.close()
+
+
+if __name__ == "__main__":
+    insta = Scraper()
+    insta.open('https://instagram.com/')
+    insta.getUserPass('pass.txt')
+    insta.login()
+    sleep(3)
+    insta.closeBrowser()
